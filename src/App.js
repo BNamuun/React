d7aa5a12f72923ca34a2cbeae66d83db16e8f20a
Evening <evening@admins-iMac.local>
2023-01-02 18:53:35 +0800

    react blog

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './style.css';
+import Header from './header';
+import Body from './body';
+import Footer from './footer';
+import Link from './link';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <Header/>
+            <Body/>
+            <Footer/>
+            <Link/>
       </header>
     </div>
   );
